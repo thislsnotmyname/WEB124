09f5e51efdf52242ac6b5586a5fc1b3053bec040
JM <143664295+thislsnotmyname@users.noreply.github.com>
2024-10-24 20:54:16 -0500

    Add color and paintbrush tags
    Later they will have the appropriate code to make them work, they are just sitting there for now

--- JS30/Paint/paint.js
+++ JS30/Paint/paint.js
@@ -3,8 +3,12 @@
 // Tutorial Code
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+
+// Mine
+canvas.height = canvas.getBoundingClientRect().height;
+canvas.width = canvas.offsetWidth;
+
+// TC
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.globalCompositeOperation = 'multiply';
@@ -12,39 +16,53 @@ ctx.globalCompositeOperation = 'multiply';
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
-let hue = 0;
-let direction = true;
 
-function draw(e) {
-    if (!isDrawing) return; // stop drawing
-    console.log(e);
-    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-    ctx.beginPath();
-
-    ctx.moveTo(lastX, lastY);
-    ctx.lineTo(e.offsetX, e.offsetY);
+const tools = {
+    currentTool: 'paintbrush',
+    paintbrush: function(e) {
+        if (!isDrawing) return; // stop drawing
+        ctx.beginPath();
     
-    ctx.stroke();
-    
-    [lastX, lastY] = [e.offsetX, e.offsetY];
-    hue++;
-    
-    if (hue >= 360) hue = 0;
-
-    if (ctx.lineWidth >= 50 || ctx.lineWidth <= 1) {
-        direction = !direction;
-    }
-    if (direction) {
-        ctx.lineWidth++;
-    } else {
-        ctx.lineWidth--;
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(e.offsetX, e.offsetY);
+        
+        ctx.stroke();
+        
+        [lastX, lastY] = [e.offsetX, e.offsetY];
     }
 }
 
-canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousemove', tools[tools.currentTool]);
 canvas.addEventListener('mousedown', (e) => {
     isDrawing = true;
     [lastX, lastY] = [e.offsetX, e.offsetY];
 });
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
+
+/*
+    original draw function:
+    if (!isDrawing) return; // stop drawing
+        console.log(e);
+        ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+        ctx.beginPath();
+    
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(e.offsetX, e.offsetY);
+        
+        ctx.stroke();
+        
+        [lastX, lastY] = [e.offsetX, e.offsetY];
+        hue++;
+        
+        if (hue >= 360) hue = 0;
+    
+        if (ctx.lineWidth >= 50 || ctx.lineWidth <= 1) {
+            direction = !direction;
+        }
+        if (direction) {
+            ctx.lineWidth++;
+        } else {
+            ctx.lineWidth--;
+        }
+*/
