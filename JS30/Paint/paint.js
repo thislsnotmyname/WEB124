9d620a1659ffdd1f0f2a2fff4e1280a0d10e00ba
JM <143664295+thislsnotmyname@users.noreply.github.com>
2024-10-30 02:48:22 -0500

    Update styles and comments; Add more icons and pages

--- JS30/Paint/paint.js
+++ JS30/Paint/paint.js
@@ -1,5 +1,5 @@
 "use strict";
-// Jeremy Meyers, 10/24/2024
+// Jeremy Meyers, 10/30/2024
 /* 
     Adapted from Wes Bos's https://javascript30.com/
     Fun with HTML5 Canvas project
@@ -7,16 +7,20 @@
 /*
     New concepts:
     Canvas API
+    
+    Challenges:
+    Canvas API
 */
-// Tutorial Code
+
+// Wes Bos
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 
-// Mine
+// My modifications
 canvas.height = canvas.getBoundingClientRect().height;
 canvas.width = canvas.offsetWidth;
 
-// TC
+// Wes Bos
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 // ctx.globalCompositeOperation = 'multiply';
@@ -25,30 +29,39 @@ let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 
-// function createOverlayCanvas() {
-//     const overlay = document.createElement('canvas');
-//     overlay.id = 'overlay';
-//     overlay.width = canvas.getBoundingClientRect().width - 10;
-//     overlay.height = canvas.getBoundingClientRect().height - 10;
-//     overlay.style.left = canvas.getBoundingClientRect().left + 'px';
-//     canvas.parentNode.insertBefore(overlay, canvas);
-//     return overlay;
-// }
-
+// My code to organize tools (adds modularity and allows for easy additions of tools)
 const TOOLS = {
     currentTool: 'paintbrush',
     paintbrush: function(e) {
-        if (!isDrawing) return; // stop drawing
+        // Wes Bos's draw function stripped to the basics
+        
+        if (!isDrawing) return;
+        
+        // console.log(e);
+        // ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+        
         ctx.beginPath();
     
         ctx.moveTo(lastX, lastY);
-        ctx.lineTo(e.offsetX + 5, e.offsetY - 10);
+        ctx.lineTo(e.offsetX, e.offsetY);
         
         ctx.stroke();
         
         [lastX, lastY] = [e.offsetX, e.offsetY];
+        
+        // hue++;
+        
+        // if (hue >= 360) hue = 0;
+    
+        // if (ctx.lineWidth >= 50 || ctx.lineWidth <= 1) {
+        //     direction = !direction;
+        // }
+        // if (direction) {
+        //     ctx.lineWidth++;
+        // } else {
+        //     ctx.lineWidth--;
+        // }
     },
-
     colorPicker: { 
         currentColor: document.querySelector('#color').value, 
         element: document.querySelector('#color'), 
@@ -56,13 +69,29 @@ const TOOLS = {
         changeBGColor: function() {
             TOOLS.colorPicker.currentColor = document.querySelector('#color').value;
             TOOLS.colorPicker.label.style.backgroundColor = TOOLS.colorPicker.currentColor;
+            
+            // Tests color picked to show a white or black version of background image for better contrast
+            if (TOOLS.colorPicker.averageOfColor(TOOLS.colorPicker.currentColor) >= 128) {
+                TOOLS.colorPicker.label.style.backgroundImage = 'url("./paint-brush-drawing-icon-black.svg")';
+            } else {
+                TOOLS.colorPicker.label.style.backgroundImage = 'url("./paint-brush-drawing-icon-white.svg")';
+            }
+
             ctx.strokeStyle = TOOLS.colorPicker.currentColor;
         },
+        averageOfColor: function(hex) {
+            const red = parseInt(`0x${hex.slice(1, 3)}`);
+            const green = parseInt(`0x${hex.slice(3, 5)}`);
+            const blue = parseInt(`0x${hex.slice(5)}`);
+
+            return (red + green + blue) / 3; // Averages rgb to determine basic lightness
+        }
     },
     stroke: {
         element: document.querySelector('#stroke-size'),
         size: document.querySelector('#stroke-size').value,
         changeStrokeWidth: function() {
+            TOOLS.stroke.size = TOOLS.stroke.element.value;
             ctx.lineWidth = TOOLS.stroke.size;
         },
     },
@@ -73,69 +102,31 @@ const TOOLS = {
         }
     },
 }
-    // rectangle: function(e) {
-    //     if (!isDrawing) return;
-    //     if (document.querySelector('#overlay')) { 
-    //         const overlay = document.querySelector('#overlay');
-    //     } else {
-    //         const overlay = createOverlayCanvas();
-    //     }
-    //     const overlayCtx = overlay.getContext('2d');
 
-    //     overlayCtx.beginPath();
-    // }
-
-
-// TC
+// Wes Bos's event listeners for drawing
 canvas.addEventListener('mousemove', (e) => TOOLS[TOOLS.currentTool](e));
 canvas.addEventListener('mousedown', (e) => {
     isDrawing = true;
-    console.log(isDrawing);
     [lastX, lastY] = [e.offsetX, e.offsetY];
 });
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
 
-// Mine
+// My event listeners for tools and selecting a coloring page
 TOOLS.colorPicker.element.addEventListener('change', TOOLS.colorPicker.changeBGColor);
 TOOLS.stroke.element.addEventListener('change', TOOLS.stroke.changeStrokeWidth);
 TOOLS.clear.element.addEventListener('mousedown', TOOLS.clear.clearCanvas);
 
 document.querySelector('#coloring-pages').addEventListener('click', (e) => {
     if (e.target.tagName === "DIV") return;
-    
-    document.querySelector('.selected-page').classList.remove('selected-page');
+    if (document.querySelector('.selected-page')) {
+        document.querySelector('.selected-page').classList.remove('selected-page');
+    }
     e.target.className = 'pages selected-page';
     canvas.style.background = `no-repeat center url(${e.target.src})`;
+    canvas.style.backgroundSize = 'contain';
 });
 
+// Initialize color picker and stroke size
 TOOLS.colorPicker.changeBGColor();
 TOOLS.stroke.changeStrokeWidth();
-
-
-/*
-    original draw function:
-    if (!isDrawing) return; // stop drawing
-        console.log(e);
-        ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-        ctx.beginPath();
-    
-        ctx.moveTo(lastX, lastY);
-        ctx.lineTo(e.offsetX, e.offsetY);
-        
-        ctx.stroke();
-        
-        [lastX, lastY] = [e.offsetX, e.offsetY];
-        hue++;
-        
-        if (hue >= 360) hue = 0;
-    
-        if (ctx.lineWidth >= 50 || ctx.lineWidth <= 1) {
-            direction = !direction;
-        }
-        if (direction) {
-            ctx.lineWidth++;
-        } else {
-            ctx.lineWidth--;
-        }
-*/
