165d46df57747ae41064438b0556031bf4cd660a
JM <143664295+thislsnotmyname@users.noreply.github.com>
2024-11-05 23:54:38 -0600

    Add Chapter 3 project

---
+++ Chapter3Project/script.js
@@ -0,0 +1,38 @@
+"use strict";
+// Jeremy Meyers, 11/06/2024
+
+const fullName = "Jeremy Meyers";
+console.log("Full name:");
+console.log(fullName);
+
+let desiredSalary = 120000;
+console.log("Desired annual salary:");
+console.log(desiredSalary);
+
+let isVeteran = false;
+console.log("Veteran status:");
+console.log(isVeteran);
+
+const namesOfFriends = [
+    "Connor Jameson",
+    "Seth Gunkel",
+    "Kade Brannin",
+];
+console.log("Names of three friends:");
+console.log(namesOfFriends);
+
+let desiredSalariesOfFriends = [
+    85000,
+    150000,
+    100000,
+];
+console.log("Desired salaries of three friends:");
+console.log(desiredSalariesOfFriends);
+
+const anotherFriend = {
+    firstName: "Matt",
+    lastName: "Argie",
+    desiredSalary: 95000,
+};
+console.log(`Name and desired salary of another friend: ${anotherFriend.firstName} ${anotherFriend.lastName}, ${anotherFriend.desiredSalary}`);
+console.log(anotherFriend);
