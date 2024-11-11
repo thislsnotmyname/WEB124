89bb55da1502297e117c084d2635b85e6e9cca12
thislsnotmyname <143664295+thislsnotmyname@users.noreply.github.com>
2024-11-11 17:22:37 -0600

    Complete tutorial

---
+++ task-list/script.js
@@ -0,0 +1,45 @@
+"use strict";
+// Jeremy Meyers, 11/11/2024
+
+// TC
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.tasks');
+const items = JSON.parse(localStorage.getItem('items')) || [];
+
+function addItem(e) {
+    e.preventDefault();
+    const text = this.querySelector('[name=item]').value;
+    const item = {
+        text,
+        done: false,
+    }
+    items.push(item);
+    populateList(itemsList, items);
+    localStorage.setItem('items', JSON.stringify(items));
+    this.reset();
+}
+
+function populateList(itemsList, items = []) {
+    const markup = items.map((item, i) => {
+        return `<li>
+            <input type="checkbox" data-index="${i}" id="item${i}" ${item.done ? "checked" : ""}>
+            <label for="item${i}">${item.text}</label>
+        </li>`;
+    }).join('');
+
+    itemsList.innerHTML = markup;
+}
+
+function toggleDone(e) {
+    if (!e.target.matches('input')) return;
+    const el = e.target;
+    const index = el.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(itemsList, items);
+}
+
+itemsList.addEventListener('click', toggleDone);
+addItems.addEventListener('submit', addItem);
+
+populateList(itemsList, items);
