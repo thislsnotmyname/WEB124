947a4c95ff509054a256623792e66a88dbde2b00
JM <143664295+thislsnotmyname@users.noreply.github.com>
2024-11-29 17:45:42 -0600

    Complete functions and do basic styling

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,2 +1,48 @@
 "use strict";
 // Jeremy Meyers, 11/29/2024
+
+function addition(input) {
+    let markup = "";
+    for (let i = 1; i <= 10; i++) {
+        markup += `${i} + ${input} = ${i + input}<br>`;
+    }
+    document.querySelector('#add').innerHTML = markup;
+}
+
+function subtraction(input) {
+    let markup = "";
+    let i = 1;
+    while (i <= 10) {
+        markup += `${i} - ${input} = ${i - input}<br>`;
+        i++;
+    }
+    document.querySelector('#sub').innerHTML = markup;
+}
+
+function multiplication(input) {
+    let markup = "";
+    let i = 1;
+    do {
+        markup += `${i} * ${input} = ${i * input}<br>`;
+        i++;
+    } while (i <= 10)
+    document.querySelector('#mult').innerHTML = markup;
+}
+
+function division(input) {
+    let markup = "";
+    for (let i = 1; i <= 10; i++) {
+        markup += `${i} / ${input} = ${(i / input).toFixed(2)}<br>`;
+    }
+    document.querySelector('#divi').innerHTML = markup;
+}
+
+function calculate() {
+    const input = Number(document.querySelector('#num').value);
+    addition(input);
+    subtraction(input);
+    multiplication(input);
+    division(input);
+}
+
+document.querySelector('#calculate').addEventListener('click', calculate);
